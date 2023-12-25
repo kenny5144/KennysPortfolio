aec8074e2a2b38a545516ccae6f1c039f0700eeb
kehinde ojo <ojok761@gmail.com>
2023-12-25 15:15:57 -0500

    changed the about sections

--- myWebsite/src/components/contact/Contact.jsx
+++ myWebsite/src/components/contact/Contact.jsx
@@ -1,18 +1,24 @@
-import React, { useRef , useState} from "react";
-import emailjs from '@emailjs/browser';
+import React, { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
 import "./contact.scss";
 
 const Contact = () => {
-  const formref =useRef();
+  const formref = useRef();
   const [error, setError] = useState(false);
   const [success, setSuccess] = useState(false);
-  const [emailInput, setEmailInput ]= useState({
-    title:"",
-    mail:"",
-    text:"",
-  })
- function handlChange(e){
-    const {name, value }=e.target
+  const [emailInput, setEmailInput] = useState({
+    title: "",
+    mail: "",
+    text: "",
+  });
+  const templateparams = {
+    form_name: emailInput.title,
+    form_email: emailInput.mail,
+    to_name: "kehinde ojo",
+    message: emailInput.text,
+  };
+  function handlChange(e) {
+    const { name, value } = e.target;
     setEmailInput((prev) => {
       return { ...prev, [name]: value };
     });
@@ -21,20 +27,21 @@ const Contact = () => {
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('service_rjb4d39', 'template_ixxga7i', formref.current, 'TSDFmMgD3XOwoXul6')
-      .then((result) => {
-         setSuccess(true)
-      }, (error) => {
-          setError(false)
-      });
-      const [name, value ]=e.target
-      console.log(emailInput)
-      setEmailInput({
-        title:"",
-        mail:"",
-        text:"",
-
-      })
+    emailjs
+      .send(
+        "service_rjb4d39",
+        "template_ixxga7i",
+        templateparams,
+        "TSDFmMgD3XOwoXul6"
+      )
+      .then(
+        (result) => {
+          setSuccess(true);
+        },
+        (error) => {
+          setError(false);
+        }
+      );
   };
   return (
     <div className="container">
@@ -55,18 +62,33 @@ const Contact = () => {
           </div>
         </div>
         <div className="formContainer">
-         <form
-         ref={formref}
-          onSubmit={sendEmail}
-          >
-            <input type="text" onChange={handlChange} 
-            value={emailInput.title}
-             required placeholder="Name" name="title" />
-            <input type="email"  onChange={handlChange} value={emailInput.mail} required placeholder="Email" name="mail" />
-            <textarea rows={8}  onChange={handlChange}  value={emailInput.text}  placeholder="Message" name="text" />
+          <form ref={formref} onSubmit={sendEmail}>
+            <input
+              type="text"
+              onChange={handlChange}
+              value={emailInput.title}
+              required
+              placeholder="Name"
+              name="title"
+            />
+            <input
+              type="email"
+              onChange={handlChange}
+              value={emailInput.mail}
+              required
+              placeholder="Email"
+              name="mail"
+            />
+            <textarea
+              rows={8}
+              onChange={handlChange}
+              value={emailInput.text}
+              placeholder="Message"
+              name="text"
+            />
             <button>Submit</button>
             {error && "Error"}
-          {success && "Success"}
+            {success && "Success"}
           </form>
         </div>
       </div>
